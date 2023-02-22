2468f8e3e4b074694c62f961af522d54f00f8ad3
APico23 <therealpico23@gmail.com>
2023-02-22 09:31:05 -0600

    Fixed the VectorValue issue

--- Divine D-Listers/Assets/VectorValue.cs
+++ Divine D-Listers/Assets/VectorValue.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public Vector2 initialValue;
     public string currentScene;
+    public Vector2 defaultValue;
+
+    public void OnAfterDeserialize()
+    {
+        initialValue = defaultValue;
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
 }
