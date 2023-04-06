d9409b9ba05bc14b625637bace81913f8578210c
APico23 <therealpico23@gmail.com>
2023-04-06 11:29:36 -0500

    Fixed Unit Stats and did some other stuff

--- Divine D-Listers/Assets/UnitStats.cs
+++ Divine D-Listers/Assets/UnitStats.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Unit Stats", menuName = "Battle/New Unit Stats")]
-public class UnitStats : ScriptableObject
+public class UnitStats : ScriptableObject, ISerializationCallbackReceiver
 {
     public Animator unitAnimator;
     public string unitName;
@@ -23,8 +24,49 @@ public class UnitStats : ScriptableObject
     public bool isDead = false;
     public bool isAttacking = false;
     public bool isHit = false;
-    public bool leveledUp = false;  
+    public bool leveledUp = false;
 
     public Sprite attack;
     public Sprite hurt;
+
+    public int defaultUnitLevel;
+
+    public int defaultExp;
+    public int defaultExpNeeded;
+
+    public int defaultDamage;
+    public int defaultLuck;
+    public int defaultDefence;
+    public int defaultSpeed;
+
+    public int defaultMaxHP;
+    public int defaultCurrentHp;
+
+
+    public void OnAfterDeserialize()
+    {
+        unitLevel = defaultUnitLevel;
+        exp = defaultExp;
+        expNeeded = defaultExpNeeded; 
+        damage = defaultDamage; 
+        luck = defaultLuck;
+        defence = defaultDefence; 
+        speed = defaultSpeed;
+        maxHP = defaultMaxHP;
+        currentHp = defaultCurrentHp;
+
+
+        isDead = false;
+        isAttacking = false;
+        isHit = false;
+        leveledUp = false;
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
+
+
+
 }
