cfd9fb9d0b1ec31d47851a5b2ef85b1d59bff80c
WysockiGabe <126089067+WysockiGabe@users.noreply.github.com>
2023-04-15 20:50:01 -0500

    pause/stats menu

--- Divine D-Listers/Assets/outOfBattleUIManager.cs
+++ Divine D-Listers/Assets/outOfBattleUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class outOfBattleUIManager : MonoBehaviour
 {
@@ -8,17 +9,22 @@ public class outOfBattleUIManager : MonoBehaviour
     public GameObject pauseMenu;
     public GameObject statsMenu;
     public GameObject bottomPanel;
+    public GameObject player;
+    playerMove playerMoveScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerMoveScript = player.GetComponent<playerMove>();
         optionsPanel.SetActive(false);
         pauseMenu.SetActive(false);
         bottomPanel.SetActive(false);
+        statsMenu.SetActive(false);
     }
 
     public void pauseButton() 
     {
+        playerMoveScript.canMove = false;
         pauseMenu.SetActive(true);
         bottomPanel.SetActive(true);
     }
@@ -29,15 +35,26 @@ public class outOfBattleUIManager : MonoBehaviour
         optionsPanel.SetActive(true);
     }
 
+    public void homeButton() 
+    {
+        SceneManager.LoadSceneAsync("Main Menu");
+    }
+
     public void statsButton()
-    { 
-    
+    {
+        statsMenu.SetActive(true);
+    }
+
+    public void statsButtonResume()
+    {
+        statsMenu.SetActive(false);
     }
 
     public void resumeButton() 
     {
         pauseMenu.SetActive(false);
         bottomPanel.SetActive(false);
+        playerMoveScript.canMove = true;
     }
 
     public void optionsBack() 
